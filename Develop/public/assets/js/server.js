795fe3df0d829197f4571984b6a196b6ff0ad265
Jason Vernot <jason.vernot@gmail.com>
2020-04-28 20:53:57 -0400

    installed nodemon. Added port listening function

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -6,3 +6,10 @@ const PORT = process.env.PORT || 1738;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+
+app.listen(PORT, () => {
+    console.log('App listening on PORT: ' + PORT);
+    console.log(`https://localhost:${PORT}`);
+  });
