db766a3568f6aac4a3b755aacdd89f2f86821d97
Jason Vernot <jason.vernot@gmail.com>
2020-04-30 22:46:08 -0400

    re-organized all files and created GET for html paths

--- server.js
+++ server.js
@@ -7,16 +7,8 @@ const PORT = process.env.PORT || 5050;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// require('./routes/apiRoutes')(app);
-// require('./routes/htmlRoutes')(app);
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
-});
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
     console.log(`App listening on PORT: ${PORT}`);
