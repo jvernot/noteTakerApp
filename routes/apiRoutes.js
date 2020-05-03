9217802127e4db6eb7290754d90975186031091d
Jason Vernot <jason.vernot@gmail.com>
2020-05-03 14:20:31 -0400

    moved routes from server.js to routes folder

---
+++ routes/apiRoutes.js
@@ -0,0 +1,56 @@
+const db = require('./../db/db.json');
+const uuidv4 = require('uuid');
+const fs = require('fs');
+
+
+module.exports = (app) => {
+    //Get Request
+    app.get("/api/notes", function(req, res) {
+    res.send(db);
+    });
+
+
+    //Post Request
+    app.post("/api/notes", function(req, res) {
+
+        let noteId = uuidv4();
+        let newNote = {
+        id: noteId,
+        title: req.body.title,
+        text: req.body.text
+        };
+
+        fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+
+        const allNotes = JSON.parse(data);
+
+        allNotes.push(newNote);
+
+        fs.writeFile("./db/db.json", JSON.stringify(allNotes, null, 2), err => {
+            if (err) throw err;
+            res.send(db);
+            console.log("Note created!")
+        });
+        });
+    });
+
+    //delete request
+    app.delete("/api/notes/:id", (req, res) => {
+
+        let noteId = req.params.id;
+
+        fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) throw err;
+
+        const allNotes = JSON.parse(data);
+        const newAllNotes = allNotes.filter(note => note.id != noteId);
+
+        fs.writeFile("./db/db.json", JSON.stringify(newAllNotes, null, 2), err => {
+            if (err) throw err;
+            res.send(db);
+            console.log("Note deleted!")
+        });
+        });
+    });
+};
--- server.js
+++ server.js
@@ -1,9 +1,4 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const db = require('./db/db.json');
-const uuidv4 = require('uuid');
-
 
 const app = express();
 
@@ -12,73 +7,8 @@ const PORT = process.env.PORT || 5050;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-//HTML ROUTES
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-
-//API ROUTES
-
-//Get Request
-app.get("/api/notes", function(req, res) {
-    res.send(db);
-});
-
-
-//Post Request
-app.post("/api/notes", function(req, res) {
-
-    let noteId = uuidv4();
-    let newNote = {
-      id: noteId,
-      title: req.body.title,
-      text: req.body.text
-    };
-
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
-      if (err) throw err;
-
-      const allNotes = JSON.parse(data);
-
-      allNotes.push(newNote);
-
-      fs.writeFile("./db/db.json", JSON.stringify(allNotes, null, 2), err => {
-        if (err) throw err;
-        res.send(db);
-        console.log("Note created!")
-      });
-    });
-});
-
-//delete request
-app.delete("/api/notes/:id", (req, res) => {
-
-    let noteId = req.params.id;
-
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
-      if (err) throw err;
-
-      const allNotes = JSON.parse(data);
-      const newAllNotes = allNotes.filter(note => note.id != noteId);
-
-      fs.writeFile("./db/db.json", JSON.stringify(newAllNotes, null, 2), err => {
-        if (err) throw err;
-        res.send(db);
-        console.log("Note deleted!")
-      });
-    });
-});
-
-
-
-
-
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 //Port Listener
 app.listen(PORT, () => {
